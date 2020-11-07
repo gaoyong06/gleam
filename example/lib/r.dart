39ffbb434f56dc6af143ed00a1cef6fe41af51d5
gaoyong <gaoyong06@qq.com>
2020-11-07 16:38:24 +0800

    增加：空状态示例empty_page.dart

---
+++ example/lib/r.dart
@@ -0,0 +1,17 @@
+/*
+ * @Author: gaoyong06@qq.com 
+ * @Date: 2020-09-24 11:06:40 
+ * @Last Modified by: gaoyong06@qq.com
+ * @Last Modified time: 2020-09-24 11:07:06
+ */
+//资源文件
+//命名规则：
+//  图标使用：ic_开头
+//  按钮使用：btn_开头
+//  背景图使用：bg_开头
+//使用示例：
+//  第一步：import 'package:shouchebao/r.dart';
+//  第二步：Image.asset(R.assetsImagesBtnPhone)
+class R {
+  static final String assetsImagesBgEmpty = 'assets/images/bg_empty.png';
+}
