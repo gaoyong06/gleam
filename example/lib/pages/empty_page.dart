38f3190728aa1f03a165c989d31094572e256cd4
gaoyong <gaoyong06@qq.com>
2020-11-07 13:57:51 +0800

    增加：example示例程序

---
+++ example/lib/pages/empty_page.dart
@@ -0,0 +1,18 @@
+/*
+ * @Author: gaoyong06@qq.com
+ * @Date: 2020-11-06 14:08:23 
+ * @Last Modified by: gaoyong06@qq.com
+ * @Last Modified time: 2020-11-06 14:08:23
+ */
+import 'package:flutter/material.dart';
+
+class EmptyPage extends StatelessWidget {
+  const EmptyPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('EmptyPage'),
+    );
+  }
+}
