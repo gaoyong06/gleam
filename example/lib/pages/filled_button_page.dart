929372ae2f2ea4bc18524b12b5acd4cc0e6d6476
gaoyong <gaoyong06@qq.com>
2020-11-13 14:04:12 +0800

    增加：FilledButton 示例程序

--- example/lib/pages/filled_button_page.dart
+++ example/lib/pages/filled_button_page.dart
@@ -4,12 +4,12 @@
  * @Last Modified by: gaoyong06@qq.com
  * @Last Modified time: 2020-11-06 14:08:23
  */
-import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gleam/gleam.dart';
+import 'package:gleam/style/app_colors.dart';
 import 'package:gleam/style/style.dart';
-import 'package:gleam_example/r.dart';
 import 'package:oktoast/oktoast.dart';
 
 /// FilledButtonPage填充按钮示例程序
@@ -24,24 +24,259 @@ class _FilledButtonPageState extends State<FilledButtonPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: ListView(
+      appBar: AppBar(
+        title: Text(
+          "FilledButton 填充按钮",
+          style: Style.ts_FFFFFF_18_bold,
+        ),
+      ),
+      body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(vertical: 0, horizontal: 16.0),
-        children: ListTile.divideTiles(context: context, tiles: [
-          //提出弹窗
-          ListTile(
-              title: Text(
-                '提示弹窗',
-                style: Style.ts_333333_15,
-              ),
-              trailing: Icon(
-                Icons.arrow_forward_ios_outlined,
-                size: 20,
-              ),
-              onTap: () {
-                showToast('确定');
-              }),
-        ]).toList(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 40.0,
+            ),
+            Text("按钮类型"),
+            SizedBox(
+              height: 16.0,
+            ),
+            //主要按钮
+            FilledButton(
+              text: '主要按钮',
+              buttonType: ButtonType.primary,
+              buttonSize: ButtonSize.normal,
+              onPressed: () {
+                showToast('主要按钮');
+              },
+            ),
+
+            SizedBox(
+              height: 16.0,
+            ),
+            //信息按钮
+            FilledButton(
+              text: '信息按钮',
+              buttonType: ButtonType.info,
+              buttonSize: ButtonSize.normal,
+              onPressed: () {
+                showToast('信息按钮');
+              },
+            ),
+
+            SizedBox(
+              height: 16.0,
+            ),
+            //默认按钮
+            FilledButton(
+              text: '默认按钮',
+              buttonType: ButtonType.defaulted,
+              buttonSize: ButtonSize.normal,
+              onPressed: () {
+                showToast('默认按钮');
+              },
+            ),
+
+            SizedBox(
+              height: 16.0,
+            ),
+            //警告按钮
+            FilledButton(
+              text: '警告按钮',
+              buttonType: ButtonType.warning,
+              buttonSize: ButtonSize.normal,
+              onPressed: () {
+                showToast('警告按钮');
+              },
+            ),
+
+            SizedBox(
+              height: 16.0,
+            ),
+            //危险按钮
+            FilledButton(
+              text: '危险按钮',
+              buttonType: ButtonType.danger,
+              buttonSize: ButtonSize.normal,
+              onPressed: () {
+                showToast('危险按钮');
+              },
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Text("加载状态"),
+            SizedBox(
+              height: 16.0,
+            ),
+
+            //加载状态(文字+loading图标)
+            FilledButton(
+              text: '加载中...',
+              width: 112.0,
+              height: 44.0,
+              buttonType: ButtonType.primary,
+              leftIcon: CupertinoActivityIndicator(),
+              onPressed: () {
+                showToast('加载状态(文字+loading图标)');
+              },
+            ),
+
+            SizedBox(
+              height: 16.0,
+            ),
+
+            //加载状态(loading图标)
+            FilledButton(
+              width: 52.0,
+              height: 44.0,
+              buttonType: ButtonType.primary,
+              leftIcon: CupertinoActivityIndicator(),
+              onPressed: () {
+                showToast('加载状态(loading图标)');
+              },
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Text("按钮形状"),
+            SizedBox(
+              height: 16.0,
+            ),
+
+            FilledButton(
+              borderRadius: 0,
+              buttonType: ButtonType.primary,
+              buttonSize: ButtonSize.normal,
+              text: "方形按钮",
+              onPressed: () {
+                showToast('方形按钮');
+              },
+            ),
+
+            SizedBox(
+              height: 16.0,
+            ),
+
+            FilledButton(
+              borderRadius: 22.0,
+              width: 88.0,
+              height: 44.0,
+              text: "圆形按钮",
+              fillColor: const Color(0xff07c160),
+              onPressed: () {
+                showToast('圆角按钮');
+              },
+            ),
+
+            SizedBox(
+              height: 16.0,
+            ),
+            Text("按钮尺寸"),
+            SizedBox(
+              height: 16.0,
+            ),
+
+            FilledButton(
+              buttonType: ButtonType.primary,
+              buttonSize: ButtonSize.large,
+              text: "大号按钮",
+              onPressed: () {
+                showToast('大号按钮');
+              },
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+
+            FilledButton(
+              buttonType: ButtonType.primary,
+              buttonSize: ButtonSize.normal,
+              text: "普通按钮",
+              onPressed: () {
+                showToast('普通按钮');
+              },
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+
+            FilledButton(
+              buttonType: ButtonType.primary,
+              buttonSize: ButtonSize.small,
+              text: "小型按钮",
+              onPressed: () {
+                showToast('小型按钮');
+              },
+            ),
+
+            SizedBox(
+              height: 16.0,
+            ),
+
+            FilledButton(
+              buttonType: ButtonType.primary,
+              buttonSize: ButtonSize.mini,
+              text: "迷你按钮",
+              onPressed: () {
+                showToast('迷你按钮');
+              },
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Text("按钮阴影"),
+            SizedBox(
+              height: 16.0,
+            ),
+            FilledButton(
+              showShadow: false,
+              width: 148.0,
+              height: 44.0,
+              text: "主要按钮(无阴影)",
+              buttonType: ButtonType.primary,
+              onPressed: () {
+                showToast('主要按钮(无阴影)');
+              },
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            FilledButton(
+              showShadow: true,
+              shadowColor: const Color(0xff07c160),
+              width: 148.0,
+              height: 44.0,
+              text: "主要按钮(有阴影)",
+              onPressed: () {
+                showToast('主要按钮(有阴影)');
+              },
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Text("禁用状态"),
+            SizedBox(
+              height: 16.0,
+            ),
+            FilledButton(
+              showShadow: true,
+              buttonSize: ButtonSize.normal,
+              disableColor: AppColors.clD8D8D8,
+              disableTextColor: AppColors.cl999999,
+              borderColor: AppColors.clD8D8D8,
+              text: "禁用状态",
+              onPressed: null,
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/src/filled_button.dart
+++ lib/src/filled_button.dart
@@ -9,19 +9,62 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 ///
 /// 内容填充按钮
-///
+/// 按钮类型
+enum ButtonType {
+  primary, //主要按钮
+  info, //信息按钮
+  defaulted, //默认按钮
+  warning, //警告按钮
+  danger //危险按钮
+}
+
+/// 按钮尺寸
+enum ButtonSize {
+  large, //大号按钮
+  normal, //普通按钮
+  small, //默认按钮
+  mini, //迷你按钮
+}
+
+///默认填充颜色
+Color defaultFillColor = const Color(0xff07c160);
+
+///默认填充颜色
+double defaultFontSize = 14.0;
+
+///默认边框颜色
+Color defaultBorderColor = Colors.transparent;
+
+///默认字体颜色
+Color defaultTextColor = Colors.white;
+
+///默认按钮内边距
+EdgeInsetsGeometry defaultPadding = EdgeInsets.symmetric(horizontal: 15.0);
+
 class FilledButton extends StatelessWidget {
+  //按钮类型
+  final ButtonType buttonType;
+
+  //按钮尺寸
+  final ButtonSize buttonSize;
+
   //按钮宽度
   final double width;
 
   //按钮高度
   final double height;
 
+  //内边距
+  final EdgeInsetsGeometry padding;
+
   //文字大小
   final double fontSize;
 
   //圆角弧度
   final double borderRadius;
+
+  //边框颜色
+  final Color borderColor;
 
   //点击回调
   final VoidCallback onPressed;
@@ -34,6 +77,9 @@ class FilledButton extends StatelessWidget {
 
   //文字颜色
   final Color textColor;
+
+  //字重
+  final FontWeight fontWeight;
 
   //填充颜色
   final Color fillColor;
@@ -50,17 +96,26 @@ class FilledButton extends StatelessWidget {
   //是否显示阴影
   final bool showShadow;
 
+  //阴影颜色
+  final Color shadowColor;
+
   const FilledButton({
-    @required this.text,
+    this.buttonType,
+    this.buttonSize,
+    this.text,
     this.width,
     this.height,
-    this.fontSize = 15,
-    this.textColor = Colors.white,
+    this.padding,
+    this.fontSize,
+    this.textColor,
+    this.fontWeight = FontWeight.normal,
     @required this.onPressed,
     this.margin,
     this.showShadow = true,
-    this.borderRadius = 25,
-    this.fillColor = const Color(0xFF5791F3),
+    this.shadowColor = const Color(0x7D80AEFF),
+    this.borderRadius = 2,
+    this.borderColor,
+    this.fillColor,
     this.disableColor = const Color(0xFFD8D8D8),
     this.disableTextColor = Colors.white,
     this.leftIcon,
@@ -68,17 +123,91 @@ class FilledButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color tempFillColor;
+    Color tempBorderColor;
+    double tempWidth;
+    double tempHeight;
+    double tempFontSize;
+    Color tempTextColor;
+    EdgeInsetsGeometry tempPadding;
+
+    switch (buttonType) {
+      case ButtonType.primary:
+        tempFillColor = const Color(0xff07c160);
+        tempBorderColor = tempFillColor;
+        tempTextColor = defaultTextColor;
+        break;
+
+      case ButtonType.info:
+        tempFillColor = const Color(0xff1989fa);
+        tempBorderColor = tempFillColor;
+        tempTextColor = defaultTextColor;
+        break;
+
+      case ButtonType.defaulted:
+        tempFillColor = const Color(0xffffffff);
+        tempBorderColor = const Color(0xffebedf0);
+        tempTextColor = const Color(0xff323233);
+        break;
+
+      case ButtonType.warning:
+        tempFillColor = const Color(0xffff976a);
+        tempBorderColor = tempFillColor;
+        tempTextColor = defaultTextColor;
+        break;
+
+      case ButtonType.danger:
+        tempFillColor = const Color(0xffee0a24);
+        tempBorderColor = tempFillColor;
+        tempTextColor = defaultTextColor;
+        break;
+
+      default:
+        tempFillColor = defaultFillColor;
+        tempBorderColor = tempFillColor;
+        tempTextColor = defaultTextColor;
+    }
+
+    switch (buttonSize) {
+      case ButtonSize.large:
+        tempWidth = 328.0;
+        tempHeight = 50.0;
+        tempFontSize = 16.0;
+        break;
+
+      case ButtonSize.normal:
+        tempWidth = 88.0;
+        tempHeight = 44.0;
+        tempFontSize = 14.0;
+        tempPadding = defaultPadding;
+        break;
+
+      case ButtonSize.small:
+        tempWidth = 66.0;
+        tempHeight = 32.0;
+        tempFontSize = 12.0;
+        tempPadding = EdgeInsets.symmetric(horizontal: 8.0);
+        break;
+
+      case ButtonSize.mini:
+        tempWidth = 58.0;
+        tempHeight = 24.0;
+        tempFontSize = 10.0;
+        tempPadding = EdgeInsets.symmetric(horizontal: 4.0);
+        break;
+    }
+
     return Container(
-      width: width ?? ScreenUtil().setWidth(335),
-      height: height ?? ScreenUtil().setWidth(44),
+      width: width ?? tempWidth ?? ScreenUtil().setWidth(88),
+      height: height ?? tempHeight ?? ScreenUtil().setWidth(44),
       margin: margin,
       decoration: BoxDecoration(
-          color: fillColor,
+          color: fillColor ?? tempFillColor ?? defaultFillColor,
           borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
           boxShadow: showShadow && onPressed != null
               ? [
                   BoxShadow(
-                    color: const Color(0x7D80AEFF),
+                    color: shadowColor,
                     offset: Offset(0.0, 5),
                     blurRadius: 20.0,
                     spreadRadius: 0.0,
@@ -86,17 +215,27 @@ class FilledButton extends StatelessWidget {
                 ]
               : []),
       child: FlatButton(
+        padding: padding ?? tempPadding ?? defaultPadding,
         shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(borderRadius))),
+          side: BorderSide(
+            color: borderColor ?? tempBorderColor ?? defaultBorderColor,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+        ),
         onPressed: onPressed,
         child: Row(mainAxisSize: MainAxisSize.min, children: [
           leftIcon ?? Container(),
-          SizedBox(width: leftIcon == null ? 0 : 5),
-          Text(text ?? "",
-              style: TextStyle(
-                  fontSize: fontSize, fontWeight: FontWeight.w600, height: 1))
+          SizedBox(width: leftIcon == null || text == null ? 0 : 5),
+          Expanded(
+            child: Text(text ?? "",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: fontSize ?? tempFontSize ?? defaultFontSize,
+                    fontWeight: fontWeight,
+                    height: 1)),
+          ),
         ]),
-        textColor: textColor,
+        textColor: textColor ?? tempTextColor ?? defaultTextColor,
         disabledTextColor: disableTextColor,
         color: Colors.transparent,
         disabledColor: disableColor,
