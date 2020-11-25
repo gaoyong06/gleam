1e48403117907ba782fd7c572c8d37ed2d31bc99
feisun <feisun@pateo.com.cn>
2020-11-25 13:55:54 +0800

    创建checkbox

---
+++ lib/src/gleam_check_box.dart
@@ -0,0 +1,17 @@
+/* 
+* created by 1129502088@qq.com
+* Date 2020/11/25 11:24
+*/
+import 'package:flutter/material.dart';
+
+class GleamCheckBox extends StatefulWidget {
+  @override
+  _GleamCheckBoxState createState() => _GleamCheckBoxState();
+}
+
+class _GleamCheckBoxState extends State<GleamCheckBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("GleamCheckBox");
+  }
+}
